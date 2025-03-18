1f5c9ca14ca96e77a0071b677faf9ecd38c61080
JHON GABRIEL OSPINA OROZCO <jhon.ospina@davivienda.com>
2025-03-18 11:23:57 -0500

    Se adicionan las card en miniatura

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
 export class MiniMapComponent {
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
 
+
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-74.10682678667843, 4.6086820158736685);
+
+  ngAfterViewInit() {
+    if ( !this.divMap?.nativeElement) throw "Map Div not found";
+    if( !this.lngLat ) throw "LngLat can't be null";
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false,
+    });
+
+    new Marker()
+      .setLngLat( this.lngLat)
+      .addTo(map)
+  }
 }
--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,6 +1,16 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Map, LngLat, Marker } from 'mapbox-gl';
 
+interface MarkerAndColor {
+  color: string;
+  marker: Marker;
+}
+
+interface PlainMarker{
+  color: string;
+  lngLat: number[]
+}
+
 @Component({
   selector: 'app-markers-page',
   templateUrl: './markers-page.component.html',
@@ -9,6 +19,8 @@ import { Map, LngLat, Marker } from 'mapbox-gl';
 export class MarkersPageComponent {
 
   @ViewChild('map') divMap?: ElementRef;
+
+  public markers: MarkerAndColor[] = [];
   public map?: Map;
   public currentLngLat: LngLat = new LngLat(-74.10682678667843, 4.6086820158736685);
 
@@ -23,6 +35,8 @@ export class MarkersPageComponent {
       zoom: 13, // starting zoom
     });
 
+    this.readFormLocalStorage();
+
     //! Este código sirve para personalizar los marcadores
     // const markerHtml = document.createElement('div');
     // markerHtml.innerHTML = 'Verona II';
@@ -34,6 +48,74 @@ export class MarkersPageComponent {
     // })
     //   .setLngLat( this.currentLngLat )
     //   .addTo( this.map! );
+  }
+
+  createMarker() {
+
+    if( !this.map ) return;
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lngLat = this.map.getCenter();
+    this.addMarker(lngLat, color);
+  }
+
+  addMarker( lngLat: LngLat, color: string ) {
+    if (!this.map ) return;
+
+    const marker = new Marker({
+      color: color,
+      draggable: true,
+    })
+
+    .setLngLat( lngLat )
+    .addTo( this.map );
+
+    this.markers.push({ color, marker});
+    this.saveToLocalStorage();
+
+     // Escuchar el evento dragend
+    marker.on('dragend', () => {
+    const index = this.markers.findIndex(m => m.marker === marker);
+    if (index !== -1) {
+      this.markers[index].marker.setLngLat(marker.getLngLat());
+      this.saveToLocalStorage();
+      }
+    });
+  }
+
+  deleteMarker(index: number) {
+    this.markers[index].marker.remove();
+    this.markers.splice( index, 1)
+  }
+
+  flyTo( marker: Marker) {
+    this.map?.flyTo({
+      zoom: 14,
+      center: marker.getLngLat()
+    });
+  }
+
+
+  saveToLocalStorage() {
+    const plainMarkers: PlainMarker[] = this.markers.map( ({color, marker}) => {
+      return {
+        color,
+        lngLat: marker.getLngLat().toArray()
+      }
+    });
+
+    localStorage.setItem('plainMarkers', JSON.stringify( plainMarkers ));
+  }
+
+  readFormLocalStorage() {
+    const plainMarkersString = localStorage.getItem('plainMarkers') ?? '[]';
+    const plainMarkers: PlainMarker[] = JSON.parse( plainMarkersString ); //! Ojo
+
+    plainMarkers.forEach( ({color, lngLat}) => {
+      const [ lng, lat] = lngLat;
+      const coords = new LngLat( lng, lat)
+      this.addMarker(coords, color)
+    })
   }
 
 
