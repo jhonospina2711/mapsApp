7dcfa72b977b537cd780368ce71adda160cb2c64
JHON GABRIEL OSPINA OROZCO <jhon.ospina@davivienda.com>
2025-03-17 22:54:46 -0500

    Se adicionan marcadores

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'app-markers-page',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './markers-page.component.css'
 })
 export class MarkersPageComponent {
+
+  @ViewChild('map') divMap?: ElementRef;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-74.10682678667843, 4.6086820158736685);
+
+  ngAfterViewInit(): void {
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado'
+
+    this.map = new Map({
+
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 13, // starting zoom
+    });
+
+    //! Este código sirve para personalizar los marcadores
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Verona II';
+
+
+    // const marker = new Marker({
+    // color: 'red',
+    //   element: markerHtml,
+    // })
+    //   .setLngLat( this.currentLngLat )
+    //   .addTo( this.map! );
+  }
+
+
 
 }
--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,33 +1,34 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import {Map} from 'mapbox-gl'
+import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import {LngLat, Map} from 'mapbox-gl'
 
 @Component({
   selector: 'app-zoom-range-page',
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css'
 })
-export class ZoomRangePageComponent implements AfterViewInit {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('map') divMap?: ElementRef;
-
-
   public zoom: number = 10;
   public map?: Map;
-
-
+  public currentLngLat: LngLat = new LngLat(-74.10682678667843, 4.6086820158736685);
 
   ngAfterViewInit(): void {
-  if ( !this.divMap ) throw 'El elemento HTML no fue encontrado'
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado'
 
-  this.map = new Map({
+    this.map = new Map({
 
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
 
     this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
   }
 
 
@@ -40,6 +41,12 @@ export class ZoomRangePageComponent implements AfterViewInit {
     this.map.on('zoomend', (ev) => {
       if (this.map!.getZoom() < 18) return;
       this.map!.zoomTo(18);
+    });
+    //Se dispara cuando el mapa se mueve
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter();
+      const { lng, lat} = this.currentLngLat;
+      //console.log({ lng, lat });
     });
   }
 
